6af0ca48035ef251a53a8779fefcd68428f1394e
naufalsatya <159998541+naufalsatya@users.noreply.github.com>
2024-08-21 15:35:20 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,27 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const title = document.querySelector('.title');
-  const text = ' special for u'.split('');
-  const backgroundMusic = document.getElementById('backgroundMusic');
+const title = document.querySelector('.title');
+const text = ' special for u'.split('');
+const backgroundMusic = document.getElementById('backgroundMusic');
 
-  // Mengatur musik latar agar diputar otomatis
+// Menambahkan pemutaran musik latar saat halaman dimuat
+window.addEventListener('load', () => {
   backgroundMusic.volume = 0.5; // Atur volume jika diperlukan
   backgroundMusic.play().catch(error => {
     console.log('Autoplay is blocked. User interaction required.');
   });
+});
 
-  // Mengisi elemen title dengan huruf-huruf dari teks
-  text.forEach(char => {
-    if (char !== ' ') {
-      title.innerHTML += `<span>${char}</span>`;
-    } else {
-      title.innerHTML += `<span style='margin-right: 20px;'></span>`;
-    }
-  });
+// Mengisi elemen title dengan huruf-huruf dari teks
+for (let index = 0; index < text.length; index++) {
+  if (text[index] !== ' ') {
+    title.innerHTML += `<span>${text[index]}</span>`;
+  } else {
+    title.innerHTML += `<span style='margin-right: 20px;'></span>`;
+  }
+}
 
-  // Menambahkan delay animasi acak pada setiap elemen span
-  const textElements = document.querySelectorAll('.title span');
-  textElements.forEach(element => {
-    const randomDelay = Math.random() * 3; // Delay acak antara 0 hingga 3 detik
-    element.style.animationDelay = `${randomDelay}s`;
-  });
+// Menambahkan delay animasi acak pada setiap elemen span
+const textElements = document.querySelectorAll('.title span');
+textElements.forEach((element) => {
+  const randomDelay = Math.random() * 3; // Delay acak antara 0 hingga 3 detik
+  element.style.animationDelay = `${randomDelay}s`;
 });
